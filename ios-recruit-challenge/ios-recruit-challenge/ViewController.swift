41f06ce3fde999d4d7952100a8c3f7fb0e45dabb
Thiago Alexandre Araújo Santiago <thiago.santiago@easynvest.com.br>
2018-02-05 21:25:22 -0200

    Create the project

---
+++ ios-recruit-challenge/ios-recruit-challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-recruit-challenge
+//
+//  Created by Thiago Alexandre Araújo Santiago on 05/02/2018.
+//  Copyright © 2018 Thiago Alexandre Araújo Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios-recruit-challenge/ios-recruit-challengeTests/ios_recruit_challengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_recruit_challengeTests.swift
+//  ios-recruit-challengeTests
+//
+//  Created by Thiago Alexandre Araújo Santiago on 05/02/2018.
+//  Copyright © 2018 Thiago Alexandre Araújo Santiago. All rights reserved.
+//
+
+import XCTest
+@testable import ios_recruit_challenge
+
+class ios_recruit_challengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
