7a91f5be3d48f5f8727ee03224749d8ac596f626
Thiago Alexandre Araújo Santiago <thiago.santiago@easynvest.com.br>
2018-02-07 23:00:58 -0200

    Create the tab bar layout

---
+++ ios-recruit-challenge/ios-recruit-challenge/MovieHelper.swift
@@ -0,0 +1,21 @@
+//
+//  MovieHelper.swift
+//  ios-recruit-challenge
+//
+//  Created by Thiago Alexandre Araújo Santiago on 07/02/2018.
+//  Copyright © 2018 Thiago Alexandre Araújo Santiago. All rights reserved.
+//
+
+import Foundation
+import DRPLoadingSpinner
+
+class MovieHelper {
+    
+    static func configLoadingSpinner(_ spinner: DRPLoadingSpinner) {
+        spinner.colorSequence = [Color.DarkYellow]
+        spinner.lineWidth = 3.0
+        spinner.maximumArcLength = CGFloat((2 * Double.pi) - Double.pi / 4)
+        spinner.minimumArcLength = 1.0
+        spinner.startAnimating()
+    }
+}
